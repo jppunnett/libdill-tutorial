459d4ee7dc1544721d8bb7e6786515eefc1ca971
James Punnett <jppunnett@yahoo.com>
2016-11-20 13:19:18 -0400

    End of step 1

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* libdill tutorial at http://libdill.org/tutorial.html
    Build with:
-   $ cc -Wall -ldill -ldsock -o ldhello main.c
+   $ cc -Wall -ldill -ldsock -o ldhello.out main.c
 */
 #include <libdill.h>
 #include <dsock.h>
@@ -12,14 +12,26 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char const *argv[])
-{
-	int port = 5555;
-	if(argc > 1) port = atoi(argv[1]);
+int main(int argc, char const *argv[]) {
+    int port = 5555;
+    if(argc > 1) port = atoi(argv[1]);
+    /* Create an IP address to bind to all interfaces */
+    ipaddr addr;
+    int rc = ipaddr_local(&addr, NULL, port, 0);
+    assert(rc == 0);
+    /* Create a socket to aisten at addr for incoming connections */
+    int ls = tcp_listen(&addr, 10);
+    if(ls < 0) {
+        perror("Can't open listening socket.");
+        return 1;
+    }
+    /* Listen for incoming connection */
+    while(1) {
+        int s = tcp_accept(ls, NULL, -1);
+        assert(s >= 0);
+        printf("New connect. Socket id: %d\n", s);
+        hclose(s);
+    }
 
-	ipaddr addr;
-	int rc = ipaddr_local(&addr, NULL, port, 0);
-	assert(rc == 0);
-
-	return 0;
+    return 0;
 }
