0b61caa459f62b1b508b6f03f72cb293d386166c
James Punnett <jppunnett@yahoo.com>
2016-11-21 20:12:33 -0400

    End of step 4

--- main.c
+++ main.c
@@ -15,6 +15,9 @@
 /* Handler for client connection */
 void handle_conn(int );
 
+/* Ten-second deadline */
+#define DEADLINE (now() + 10000)
+
 
 int main(int argc, char const *argv[]) {
     int port = 5555;
@@ -31,12 +34,16 @@ int main(int argc, char const *argv[]) {
     }
     /* Listen for incoming connection */
     while(1) {
-        int s = tcp_accept(ls, NULL, -1);
-        assert(s >= 0);
+        int s = tcp_accept(ls, NULL, DEADLINE);
+        if(s < 0) {
+            if(errno == ETIMEDOUT)
+                continue;
+            else
+                break;
+        }
         int cr = go(handle_conn(s));
         assert(cr >= 0);
     }
-
     return 0;
 }
 
@@ -45,17 +52,21 @@ coroutine void handle_conn(int s) {
     s = crlf_start(s);
     assert(s >= 0);
     /* Send a message to the client */
-    int rc = msend(s, "Hey! What's your name?", 22, -1);
+    int rc = msend(s, "Hey! What's your name?", 22, DEADLINE);
     if(rc != 0) goto cleanup;
-    /* Receive the client's name */
+    printf("msend1: rc=%d, errno=%d\n", rc, errno);
     char name[256];
-    size_t sz = mrecv(s, name, sizeof(name), -1);
-    if(sz < 0) goto cleanup;
+    /* Receive the client's name */
+    size_t sz = mrecv(s, name, sizeof(name), DEADLINE);
+    printf("mrecv: sz=%zu, errno=%d\n", sz, errno);
+    /* It's possible for sz >= 0 and for errno to != 0 */
+    if(sz < 0 || errno) goto cleanup;
     name[sz] = 0;
     /* Send the client a polite greeting */
     char greeting[256];
     rc = snprintf(greeting, sizeof(greeting), "Hola, %s!", name);
-    msend(s, greeting, rc, -1);
+    rc = msend(s, greeting, rc, DEADLINE);
+    printf("msend2: rc=%d, errno=%d\n", rc, errno);
 
 cleanup:
     rc = hclose(s);
