cf3ed2bfe230b43615a1e3edab1064020542797e
James Punnett <jppunnett@yahoo.com>
2019-08-04 17:45:00 -0400

    Replace channel with chmake.

--- boring10.c
+++ boring10.c
@@ -8,7 +8,7 @@
 #include <assert.h>
 #include <time.h>
 
-coroutine void boring(int start_from, int ch, int quitch)
+coroutine void boring(int start_from, int ch, int quit_ch[2])
 {
     printf("Entering boring()\n");
 
@@ -17,27 +17,28 @@ coroutine void boring(int start_from, int ch, int quitch)
 
     struct chclause clauses[] = {
         {CHSEND, ch, &start_from, sizeof(start_from)},
-        {CHRECV, quitch, &done, sizeof(done)}
+        {CHRECV, quit_ch[0], &done, sizeof(done)}
     };
 
     while(!done) {
         rc = choose(clauses, 2, -1);
-        assert(rc >= 0);
+        assert(rc >= 0 && errno == 0);
         ++start_from;
     }
 
     printf("Leaving boring()\n");
-    rc = chsend(quitch, &done, sizeof(done), -1);
+    rc = chsend(quit_ch[1], &done, sizeof(done), -1);
     assert(rc == 0);
 }
 
-int boring_gen(int start_from, int quitch)
+int boring_gen(int start_from, int quit_ch[2])
 {
-    int ch = channel(sizeof(int), 0);
-    assert(ch >= 0);
-    int cr = go(boring(start_from, ch, quitch));
-    assert(cr >= 0);
-    return ch;
+    int ch[2];
+    int rc = chmake(ch);
+    assert(rc == 0);
+    int bundle = go(boring(start_from, ch[1], quit_ch));
+    assert(bundle >= 0);
+    return ch[0];
 }
 
 int main(int argc, char const *argv[])
@@ -45,30 +46,34 @@ int main(int argc, char const *argv[])
     srand(time(NULL));
     printf("Starting.\n");
 
-    int quitch = channel(sizeof(int), 0);
-    assert(quitch >= 0);
-
-    int joech = boring_gen(1, quitch);
-    assert(joech >= 0);
-
     int rc;
-    int joe_count = 0;
-    int max =  rand() % 10;
-    printf("max: %d\n", max);
-
-    for (int i = 0; i < max; ++i)
-    {
-        rc = chrecv(joech, &joe_count, sizeof(joe_count), -1);
-        assert(rc == 0);
-        printf("Joe's count: %d\n", joe_count);
-    }
-
-    int done = 1;
-    rc = chsend(quitch, &done, sizeof(done), -1);
+    
+    int quit_ch[2];
+    rc = chmake(quit_ch);
     assert(rc == 0);
 
-    /* Wait on boring() to cleanup */
-    rc = chrecv(quitch, &done, sizeof(done), -1);
+    int boring_ch = boring_gen(1, quit_ch);
+    assert(boring_ch >= 0);
+
+    int boring_count = 0;
+    int max =  rand() % 10;
+    printf("max: %d\n", max);
+    
+    int i;
+    for (i = 0; i < max; ++i)
+    {
+        rc = chrecv(boring_ch, &boring_count, sizeof(boring_count), -1);
+        assert(rc == 0);
+        printf("Boring count: %d\n", boring_count);
+    }
+
+    /* Tell boring coroutine to finish up */
+    int done = 1;
+    rc = chsend(quit_ch[1], &done, sizeof(done), -1);
+    assert(rc == 0);
+
+    /* Wait on boring coroutine to confirm they cleaned up */
+    rc = chrecv(quit_ch[0], &done, sizeof(done), -1);
     assert(rc == 0);
 
     printf("Done.\n");
