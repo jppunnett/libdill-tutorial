6d236f4f09a3a12982d03687abedfc30b9541d91
James Punnett <jppunnett@yahoo.com>
2019-08-04 17:14:45 -0400

    Replace channel with chmake.

--- boring6.c
+++ boring6.c
@@ -1,5 +1,5 @@
 /* Little boring program to learn how libdill supports concurrency.
-   This one uses select/choose */
+   This one uses choose */
 
 #include <libdill.h>
 #include <stdio.h>
@@ -12,18 +12,19 @@ coroutine void boring(int start_from, int ch)
     while(1) {
         int rc = chsend(ch, &start_from, sizeof(start_from), -1);
         assert(rc == 0);
-        msleep(now() + (500 + (rand() % 1000)));
+        msleep(now() + rand() % 1000);
         ++start_from;
     }
 }
 
 int boring_gen(int start_from)
 {
-    int ch = channel(sizeof(int), 0);
-    assert(ch >= 0);
-    int cr = go(boring(start_from, ch));
-    assert(cr >= 0);
-    return ch;
+    int ch[2];
+    int rc = chmake(ch);
+    assert(rc == 0);
+    int hbundle = go(boring(start_from, ch[1]));
+    assert(hbundle >= 0);
+    return ch[0];
 }
 
 int main(int argc, char const *argv[])
@@ -44,9 +45,10 @@ int main(int argc, char const *argv[])
         {CHRECV, ann, &ann_count, sizeof(ann_count)}
     };
     
-    for(int i = 0; i < 10; ++i) {
+    int i;
+    for(i = 0; i < 10; ++i) {
         int rc = choose(clauses, 2, -1);
-        assert(rc >= 0);
+        assert(rc >= 0 && errno == 0);
         int *count = clauses[rc].val;
         assert(count);
         printf("Count %d\n", *count);
