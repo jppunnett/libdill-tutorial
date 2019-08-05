e288693e54242248485af5fe58f03b107636a667
James Punnett <jppunnett@yahoo.com>
2019-08-05 09:04:31 -0400

    Replace channel with chmake.

--- boring9.c
+++ boring9.c
@@ -7,32 +7,33 @@
 #include <assert.h>
 #include <time.h>
 
-coroutine void boring(int start_from, int ch, int quitch)
+coroutine void boring(int start_from, int ch, int quit_ch)
 {
     printf("Entering boring()\n");
 
     int done = 0;
     struct chclause clauses[] = {
         {CHSEND, ch, &start_from, sizeof(start_from)},
-        {CHRECV, quitch, &done, sizeof(done)}
+        {CHRECV, quit_ch, &done, sizeof(done)}
     };
     
     while(!done) {
         int rc = choose(clauses, 2, -1);
-        assert(rc >= 0);
+        assert(rc >= 0 && errno == 0);
         ++start_from;
     }
 
     printf("Leaving boring()\n");
 }
 
-int boring_gen(int start_from, int quitch)
+int boring_gen(int start_from, int quit_ch)
 {
-    int ch = channel(sizeof(int), 0);
-    assert(ch >= 0);
-    int cr = go(boring(start_from, ch, quitch));
-    assert(cr >= 0);
-    return ch;
+    int ch[2];
+    int rc = chmake(ch);
+    assert(rc == 0);
+    int bundle = go(boring(start_from, ch[1], quit_ch));
+    assert(bundle >= 0);
+    return ch[0];
 }
 
 int main(int argc, char const *argv[])
@@ -40,27 +41,29 @@ int main(int argc, char const *argv[])
     srand(time(NULL));
     printf("Starting.\n");
 
-    int quitch = channel(sizeof(int), 0);
-    assert(quitch >= 0);
-
-    int joech = boring_gen(10, quitch);
-    assert(joech >= 0);
-
     int rc;
-    int joe_count = 0;
+
+    int quit_ch[2];
+    rc = chmake(quit_ch);
+    assert(rc == 0);
+
+    int boring_ch = boring_gen(10, quit_ch[0]);
+    assert(boring_ch >= 0);
+
+    int boring_count = 0;
     int max =  rand() % 10;
     printf("max: %d\n", max);
-
-    for (int i = 0; i < max; ++i)
-    {
-        rc = chrecv(joech, &joe_count, sizeof(joe_count), -1);
+    
+    int i;
+    for(i = 0; i < max; ++i) {
+        rc = chrecv(boring_ch, &boring_count, sizeof(boring_count), -1);
         assert(rc == 0);
-        printf("Joe's count: %d\n", joe_count);
+        printf("Boring count: %d\n", boring_count);
     }
 
     /* Any non-zero value will end the loop in boring() */
     int done = 1;
-    rc = chsend(quitch, &done, sizeof(done), -1);
+    rc = chsend(quit_ch[1], &done, sizeof(done), -1);
     assert(rc == 0);
 
     /* This pause gives boring() enough time to exit */
