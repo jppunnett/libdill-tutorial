f9c2ae20079fbbca5a6503d215465a158ee94a9f
James Punnett <jppunnett@yahoo.com>
2019-10-06 14:19:27 +0000

    Update to use latest libdill version.

--- gpm/du2.c
+++ gpm/du2.c
@@ -8,7 +8,7 @@
 #include <libdill.h>
 
 /* Channel to communicate file sizes */
-int filesz_ch = -1;
+int filesz_ch[2];
 
 static int
 recordsz(const char *fpath, const struct stat *sb,
@@ -18,7 +18,7 @@ recordsz(const char *fpath, const struct stat *sb,
     int rc = 0;
     if(tflag == FTW_F) {
         fsize = sb->st_size;
-        rc = chsend(filesz_ch, &fsize, sizeof(fsize), -1);
+        rc = chsend(filesz_ch[1], &fsize, sizeof(fsize), -1);
         if(rc != 0) return 1;
     }
     return 0;
@@ -36,7 +36,7 @@ walkdirs(char *dirs[], int ndirs)
             exit(EXIT_FAILURE);
         }
     }
-    rc = chdone(filesz_ch);
+    rc = chdone(filesz_ch[1]);
     if(rc != 0) {
         perror("chdone");
         exit(EXIT_FAILURE);
@@ -62,16 +62,24 @@ time_startTick(int ch, int64_t interval)
 int
 time_Tick(int64_t interval)
 {
-    int ch = channel(sizeof(int64_t), 1);
-    if(ch == -1) return -1;
+    int ch[2];
+    int rc = chmake(ch);
+    if(rc != 0) {
+	    perror("Could not make timer channel.");
+	    return -1;
+    }
     /* An interval of zero means never tick, but we return the channel so the
        caller can still receive on the channel. In this case it will never 
        receive any tick. */
-    if(interval == 0) return ch;
+    if(interval == 0) return ch[0];
 
-    int rc = go(time_startTick(ch, interval));
-    if(rc == -1) return -1;
-    return ch;
+    rc = go(time_startTick(ch[1], interval));
+    if(rc < 0) {
+	    perror("Could not start ticker coroutine.");
+	    return -1;
+    }
+
+    return ch[0];
 }
 
 void
@@ -94,6 +102,7 @@ main(int argc, char *argv[])
             verbose = 1;
             break;
         }
+	// TODO: Add help option
     }
     /* Default to current directory if no directories provided */
     char **roots = curdir;
@@ -102,17 +111,22 @@ main(int argc, char *argv[])
         roots = &argv[optind];
         ndirs = argc - optind;   
     }
+
+    int rc;
+
     /* Start coroutine to traverse directories */
-    filesz_ch = channel(sizeof(int64_t), 1);
-    if(filesz_ch < 0) {
-        perror("channel");
+    rc = chmake(filesz_ch);
+    if(rc != 0) {
+        perror("Cannot make file size channel.");
         exit(EXIT_FAILURE);
     }
-    int rc = go(walkdirs(roots, ndirs));
-    if(rc < 0) {
+    
+    int bundle = go(walkdirs(roots, ndirs));
+    if(bundle < 0) {
         perror("go");
         exit(EXIT_FAILURE);
     }
+
     /* Start ticker to periodically display results */
     int ticker_ch = time_Tick(verbose ? 500 : 0);
     if(ticker_ch < 0) {
@@ -122,7 +136,7 @@ main(int argc, char *argv[])
     int64_t size = 0;
     int64_t tick = 0;
     struct chclause clauses[] = {
-        {CHRECV, filesz_ch, &size, sizeof(size)},
+        {CHRECV, filesz_ch[0], &size, sizeof(size)},
         {CHRECV, ticker_ch, &tick, sizeof(tick)}
     };
     
