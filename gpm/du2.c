201b77224ae61c5a0493d76246997905e26c685a
James Punnett <jppunnett@yahoo.com>
2019-10-08 10:58:45 +0000

    Formatting.

--- gpm/du2.c
+++ gpm/du2.c
@@ -14,122 +14,120 @@ int filesz_ch[2];
 
 static int
 recordsz(const char *fpath, const struct stat *sb,
-         int tflag, struct FTW *ftwbuf)
+	 int tflag, struct FTW *ftwbuf)
 {
-    int64_t fsize = 0;
-    int rc = 0;
-    if(tflag == FTW_F) {
-        fsize = sb->st_size;
-        rc = chsend(filesz_ch[1], &fsize, sizeof(fsize), -1);
-        if(rc != 0) return 1;
-    }
-    return 0;
+	int64_t fsize = 0;
+	int rc = 0;
+	if (tflag == FTW_F) {
+		fsize = sb->st_size;
+		rc = chsend(filesz_ch[1], &fsize, sizeof(fsize), -1);
+		if (rc != 0)
+			return 1;
+	}
+	return 0;
 }
 
-coroutine void
-walkdirs(char *dirs[], int ndirs)
+coroutine void walkdirs(char *dirs[], int ndirs)
 {
-    int rc = 0;
-    for(int i = 0; i < ndirs; ++i) {
-        rc = nftw(dirs[i], recordsz, 20, FTW_PHYS);
-        if(rc != 0) {
-            perror("nftw");
-            exit(EXIT_FAILURE);
-        }
-    }
-    rc = chdone(filesz_ch[1]);
-    if(rc != 0) {
-        perror("chdone");
-        exit(EXIT_FAILURE);
-    }
+	int rc = 0;
+	for (int i = 0; i < ndirs; ++i) {
+		rc = nftw(dirs[i], recordsz, 20, FTW_PHYS);
+		if (rc != 0) {
+			perror("nftw");
+			exit(EXIT_FAILURE);
+		}
+	}
+	rc = chdone(filesz_ch[1]);
+	if (rc != 0) {
+		perror("chdone");
+		exit(EXIT_FAILURE);
+	}
 }
 
-void
-printDirUsage(int64_t nfiles, int64_t nbytes)
+void printDirUsage(int64_t nfiles, int64_t nbytes)
 {
-    printf("%ld files\t%.1f GB\n", nfiles, nbytes / 1e9);
+	printf("%ld files\t%.1f GB\n", nfiles, nbytes / 1e9);
 }
 
 
-char *curdir[] = {"."};
+char *curdir[] = { "." };
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int verbose = 0;
-    int c;
-    while((c = getopt(argc, argv, "v")) != -1) {
-        switch(c) {
-        case 'v':
-            verbose = 1;
-            break;
-        }
-    }
-    
-    /* Default to current directory if no directories provided */
-    char **roots = curdir;
-    int ndirs = 1;
-    if(optind < argc) {
-        roots = &argv[optind];
-        ndirs = argc - optind;   
-    }
+	int verbose = 0;
+	int c;
+	while ((c = getopt(argc, argv, "v")) != -1) {
+		switch (c) {
+		case 'v':
+			verbose = 1;
+			break;
+		}
+	}
 
-    int rc;
+	/* Default to current directory if no directories provided */
+	char **roots = curdir;
+	int ndirs = 1;
+	if (optind < argc) {
+		roots = &argv[optind];
+		ndirs = argc - optind;
+	}
 
-    /* Start coroutine to traverse directories */
-    rc = chmake(filesz_ch);
-    if(rc != 0) {
-        perror("Cannot make file size channel.");
-        exit(EXIT_FAILURE);
-    }
-    
-    int bundle = go(walkdirs(roots, ndirs));
-    if(bundle < 0) {
-        perror("go");
-        exit(EXIT_FAILURE);
-    }
+	int rc;
 
-    /* Start ticker to periodically display results */
-    int ticker_ch = TickEvery(verbose ? 500 : 0);
-    if(ticker_ch < 0) {
-        perror("time_Tick");
-        exit(EXIT_FAILURE);
-    }
+	/* Start coroutine to traverse directories */
+	rc = chmake(filesz_ch);
+	if (rc != 0) {
+		perror("Cannot make file size channel.");
+		exit(EXIT_FAILURE);
+	}
 
-    int64_t size = 0;
-    int64_t tick = 0;
-    struct chclause clauses[] = {
-        {CHRECV, filesz_ch[0], &size, sizeof(size)},
-        {CHRECV, ticker_ch, &tick, sizeof(tick)}
-    };
-    
-    int64_t nfiles = 0, nbytes = 0;
-    int done = 0;
-    while(!done) {
-        rc = choose(clauses, 2, -1);
-        switch(rc) {
-        case 0:
-            if(errno == EPIPE) {
-                /* Channel closed by sending coroutine */
-                done = 1;
-                break;
-            } else {
-                if(errno != 0) {
-                    perror("choose");
-                    exit(EXIT_FAILURE);
-                }
-            }
-            nfiles++;
-            nbytes += size;
-            break;
-        case 1:
-            printDirUsage(nfiles, nbytes);
-            break;
-        case -1:
-            perror("choose");
-            exit(EXIT_FAILURE);
-        }
-    }
-    printDirUsage(nfiles, nbytes);
-    exit(EXIT_SUCCESS);
+	int bundle = go(walkdirs(roots, ndirs));
+	if (bundle < 0) {
+		perror("go");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Start ticker to periodically display results */
+	int ticker_ch = TickEvery(verbose ? 500 : 0);
+	if (ticker_ch < 0) {
+		perror("time_Tick");
+		exit(EXIT_FAILURE);
+	}
+
+	int64_t size = 0;
+	int64_t tick = 0;
+	struct chclause clauses[] = {
+		{CHRECV, filesz_ch[0], &size, sizeof(size)},
+		{CHRECV, ticker_ch, &tick, sizeof(tick)}
+	};
+
+	int64_t nfiles = 0, nbytes = 0;
+	int done = 0;
+	while (!done) {
+		rc = choose(clauses, 2, -1);
+		switch (rc) {
+		case 0:
+			if (errno == EPIPE) {
+				/* Channel closed by sending coroutine */
+				done = 1;
+				break;
+			} else {
+				if (errno != 0) {
+					perror("choose");
+					exit(EXIT_FAILURE);
+				}
+			}
+			nfiles++;
+			nbytes += size;
+			break;
+		case 1:
+			printDirUsage(nfiles, nbytes);
+			break;
+		case -1:
+			perror("choose");
+			exit(EXIT_FAILURE);
+		}
+	}
+	printDirUsage(nfiles, nbytes);
+	exit(EXIT_SUCCESS);
 }
