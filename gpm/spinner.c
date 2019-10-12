4d76668a49c5ec2647b84de8a6a90eb80abeebb0
James Punnett <jppunnett@yahoo.com>
2019-10-12 19:11:27 +0000

    Initial commit

---
+++ gpm/spinner.c
@@ -0,0 +1,40 @@
+// Calculates the 45th Fibonacci number with a visual indicator.
+#include <stdio.h>
+#include <assert.h>
+#include <libdill.h>
+
+// Calculates Fibonacci of x.
+static int fib(int x)
+{
+	// Need to yield or spinner will not have any time to spin.
+	int rc = yield();
+	assert(rc == 0);
+
+	if (x < 2) return x;
+	return fib(x - 1) + fib(x - 2);
+}
+
+coroutine void spinner(int delay)
+{
+	const char spinChars[] = {'-', '\\', '|', '/'};
+	while (1) {
+		for (int i = 0; i < sizeof(spinChars); i++) {
+			printf("\r%c", spinChars[i]);
+			msleep(now() + delay);
+		}
+	}
+}
+
+int main()
+{
+	// Turn off buffering on stdout otherwise we won't see the spinner.
+	setbuf(stdout, NULL);
+
+	int rc = go(spinner(100));
+	assert(rc != -1);
+
+	const int n = 45;
+	int fibN = fib(n);
+	printf("\rFibonacci(%d) = %d\n", n, fibN);
+}
+
