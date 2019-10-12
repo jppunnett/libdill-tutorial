0b4900a73fb6206ad82511b2054843baec172618
James Punnett <jppunnett@yahoo.com>
2019-10-12 19:29:53 +0000

    Add note about using yield().

--- gpm/spinner.c
+++ gpm/spinner.c
@@ -1,4 +1,7 @@
 // Calculates the 45th Fibonacci number with a visual indicator.
+// Note: using yield() in fib() may allow the spinner to actually spin, but
+// fib() takes a lot longer to complete. E.g. Over 2 minutes with yield()
+// vs. 10 seconds without it.
 #include <stdio.h>
 #include <assert.h>
 #include <libdill.h>
@@ -30,7 +33,7 @@ int main()
 	// Turn off buffering on stdout otherwise we won't see the spinner.
 	setbuf(stdout, NULL);
 
-	int rc = go(spinner(100));
+	int rc = go(spinner(500));
 	assert(rc != -1);
 
 	const int n = 45;
