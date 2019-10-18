4958d27cf69f147acb329c17397c959f7866bcc3
James Punnett <jppunnett@yahoo.com>
2019-10-18 02:40:40 +0000

    Add log_error

--- gpm/log.h
+++ gpm/log.h
@@ -7,9 +7,16 @@
 #define get_errno() \
 	(errno == 0 ? "none" : strerror(errno))
 
-#define log_fatal(msg, ...) \
+#define log_error(msg, ...) \
 	do {\
 		fprintf(stderr, "[ERROR] %s:%d (errno: %s): " msg "\n",\
+			__FILE__, __LINE__, get_errno(), ##__VA_ARGS__);\
+		fflush(stderr);\
+	} while(0)
+
+#define log_fatal(msg, ...) \
+	do {\
+		fprintf(stderr, "[FATAL] %s:%d (errno: %s): " msg "\n",\
 			__FILE__, __LINE__, get_errno(), ##__VA_ARGS__);\
 		fflush(stderr);\
 		abort();\
